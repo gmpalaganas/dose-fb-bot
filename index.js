b2196e3b0c3b01fabc40242404ea5b2830687550
Genesis Ian Palaganas <palaganas.genesis@gmail.com>
2018-02-18 13:48:40 +0800

    Add log on what port is being listned to

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const
     app = express();
 
 
-app.listen(process.env.PORT || 8888, () => console.log('webhook is listening'));
+app.listen(process.env.PORT || 8888, () => console.log('webhook is listening ', app.get('port'));
 
 app.use(body_parser.urlencoded({extended: false;}));
 app.use(body_parser.json());
